2fee539592387a33ab80b91e9537d5e1c4b18eb3
Nganga <nganga.inc@gmail.com>
2022-09-09 15:47:35 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 
 /**
- * The function called at program startup is named main. 
- * The implementation declares no prototype for this function. 
+ * The function called at program startup is named main
+ * The implementation declares no prototype for this function
  * It shall be defined with a return type of int and with no parameters
  * This program wuses puts command to print a string
  */
