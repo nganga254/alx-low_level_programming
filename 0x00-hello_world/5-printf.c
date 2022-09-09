d7de9ed2311d0b7a7724690ec8872001a0e01d72
Nganga <nganga.inc@gmail.com>
2022-09-09 15:58:10 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/* The main function is the entry point of the program
+ * It returns a value 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return(0);
+}
