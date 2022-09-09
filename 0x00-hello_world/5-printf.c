97a9e60fab5debdcd1f5c25101ff31e61dd02794
Nganga <nganga.inc@gmail.com>
2022-09-09 16:54:02 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-/* The main function is the entry point of the program
- * It returns a value zero or EOF
- */
+/* Main : Main block
+ * Description : Print a string
+ * Return 0 */
+
 int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art, ");
 	return(0);
 }
