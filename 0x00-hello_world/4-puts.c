441fe89f8fd5e171ba9420a4399e1a4d9457d8b1
Nganga <nganga.inc@gmail.com>
2022-09-09 15:05:39 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 
-/*The entry point of the program that prints a string*/
+/* The entry point of the program that prints a string */
 int main()
 
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
