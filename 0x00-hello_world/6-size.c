fcfa99a0ef8ebd11a8eb40a8e2e78bf08d4bdf65
Nganga <nganga.inc@gmail.com>
2022-09-09 18:13:02 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,11 +2,11 @@
 
 /**
  * main : main block reurns the value 0
- * description : an algorithm to print the size of various types
- * returns 0
+ * description : an algorithm to print the size of 
+ * various types on the computer it is compiled and run on
  */
 
-int main(void)
+int main(void) /* main block returns the value 0 */
 {
 	int intType;
 	float floatType;
@@ -14,8 +14,8 @@ int main(void)
 	double doubleType;
 
 /* sizeof evaluates the value of a variable */
-	printf("The size of integer : %zu bytes/n", sizeof(intType));
-	printf("The size of float : %zu bytes/n", sizeof(floatType));
-	printf("The size of character : %zu bytes/n", sizeof(charType));
-	printf("The size of double : %zu bytes/n", sizeof(doubleType));
+	printf("The size of integer : %zu bytes\n", sizeof(intType));
+	printf("The size of float : %zu bytes\n", sizeof(floatType));
+	printf("The size of character : %zu bytes\n", sizeof(charType));
+	printf("The size of double : %zu bytes\n", sizeof(doubleType));
 }
