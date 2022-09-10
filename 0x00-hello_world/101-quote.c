95bfeea655223075ef62adb0bbb5d969a8ab4807
Nganga <nganga.inc@gmail.com>
2022-09-10 03:16:56 -0700

    Task8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/** main - Entry level
+ * description : C program that printsto the standard error
+ * return 1
+ */
+
+int main(void)
+{
+	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return(1);
+}
