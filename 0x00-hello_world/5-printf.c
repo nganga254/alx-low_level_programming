ae414ef0035da4ef42d4eb0c0546053e455f8cc0
Nganga <nganga.inc@gmail.com>
2022-09-09 17:30:19 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
+int main(void)
+
 /**
  * main - main block
  * description : a program that uses printf
  * to output a string to stdin
  * return 0
  */
-int main(void) /* main block returns 0 */
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
