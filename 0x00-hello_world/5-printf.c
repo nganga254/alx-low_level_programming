5707ccff6ab4d37c08a4483c30222912c3252d46
Nganga <nganga.inc@gmail.com>
2022-09-09 18:04:18 -0700

    Task 6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,7 +7,7 @@
  * return 0
  */
 
-int main(void) /* main block*/
+int main(void) /* main block returns the value 0 */
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main : main block reurns the value 0
+ * description : an algorithm to print the size of various types
+ * returns 0
+ */
+
+int main(void)
+{
+	int intType;
+	float floatType;
+	char charType;
+	double doublType;
+
+/* sizeof evaluates the value of a variable */
+	printf("The size of integer : %zu bytes/n", sizeof(intType));
+	printf("The size of float : %zu bytes/n", sizeof(floatType));
+	printf("The size of character : %zu bytes/n", sizeof(charType));
+	printf("The size of double : %zu bytes/n", sizeof(doubleType));
+}
