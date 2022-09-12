4492febb443dbb9f0eb84582f473ab468679685c
Nganga <nganga.inc@gmail.com>
2022-09-11 20:56:09 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* description :  print whether the number stored
+* in the variable n is positive or negative
+*
+* return 0(Always)                                                                                          
+*/
+
+int main(void)
+{
+	int x;
+	for (x = '0'; x <= '10'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+
+return (0);
+}
