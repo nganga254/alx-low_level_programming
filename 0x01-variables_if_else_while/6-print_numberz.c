425bc3674c5af09f1ce5873069c6ac029f1771fb
Nganga <nganga.inc@gmail.com>
2022-09-11 22:12:26 -0700

    Task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, function returns (0)
+ *
+ * description - Use putchar to print out all decimal numbers
+ *
+ * return 0(Always)
+ */
+
+int main(void)
+{	
+	int x;
+
+	for (x = 0 ; x <= 9 ; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+}
