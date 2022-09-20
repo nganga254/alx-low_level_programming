22fb83fa6072e72dbb6329dfa1987128e1c9f6b1
Nganga <nganga.inc@gmail.com>
2022-09-20 14:10:00 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "header.h"
+
+/**
+ * main - Entry point 
+ * 
+ * descriptioon : a function that takes a pointer to an int as parameter and
+ * updates the value it points to to 98
+ * 
+ * return 0 (Always)
+ */
+int main(void)
+{
+	int a;
+	int *ptr = &a;
+	void reset_to_98(int *ptr);
+	putchar(*ptr);
+	return (0);
+}
