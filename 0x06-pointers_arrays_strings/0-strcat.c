ea0d7b52510a0199855fc41a36d2b20915f27e15
Nganga <nganga.inc@gmail.com>
2022-09-23 14:31:20 -0700

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * MAin - Entry point
+ * Description : Concanate two strings
+ * return 0 (Always)
+ */
+
+char *_strcat();
+int main(void)
+{
+	char *_strcat();
+	return(0);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+/**
+ * Prototypes used in the functions
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *src;
+	char *dest;
+	strcat(*dest + *src);
+	return (*dest);
+}
